b74fc25e545cab46f99327afa59dee44cacb40fe
Stuart Lynn <stuart.lynn@gmail.com>
2018-06-03 14:34:27 -0400

    style changes

--- client/src/App.js
+++ client/src/App.js
@@ -17,8 +17,8 @@ class App extends Component {
   render() {
     return (
       <Container>
-        <Typography variant="display3" gutterBottom> Long Live The New Sound </Typography>
-       <Typography variant='body2' style={{marginBottom:'20px'}}>
+       <Typography variant="display3" gutterBottom> Long Live The New Sound </Typography>
+       <Typography variant='body2' style={{marginBottom:'10px'}}>
         This is a podcast for artists, archivists, producers and other interested parties to share and hear work that experiments in sound.
 
         That means we accept work from genres including:
@@ -31,20 +31,19 @@ class App extends Component {
           <li>Experimental interviews</li>
           <li>Etc...</li>
         </ul>
-        However, this isn’t a space for experimental music.
 
         We aren’t a show in the traditional sense. There is no host, no weekly theme, no ads, no clock and no release schedule. Instead this is an ongoing space where different kinds of audio is perpetually being submitted and added to the catalogue. A playlist where you download or stream for as long as you like, listen actively, passively, on your commute or in your shower. No interruptions from DJs, hosts or ad breaks.
       </Typography>
 
-      <Typography variant='display2'>
+      <Typography variant='display2' style={{marginBottom:'10px'}}>
         Submit Your Work
       </Typography>
 
       <SubmitForm />
-        <Typography variant='display2' gutterBottom>
+        <Typography variant='display2' style={{marginBottom:'10px'}} gutterBottom>
           Submission Guidelines
         </Typography>
-        <Typography variant='body2' style={{marginBottom:'20px'}}>
+        <Typography variant='body2' style={{marginBottom:'30px'}}>
           <ul>
             <li>Minimum time: 5 seconds</li>
             <li>Maximum time: 1 hour</li>
@@ -61,32 +60,16 @@ class App extends Component {
           Be sure to include your name (or artist, collective name) and a brief description or information about the work. You may also include a link to your work online, and social media links. This will appear in the episode description. We also ask that you include an email address in case we need to contact you about the work - your contact information will not be included in the podcast description unless you want it to.
         </Typography>
 
-       <Typography variant='body2' style={{marginBottom:'20px'}}>
-        This is a podcast for artists, archivists, producers and other interested parties to share and hear work that experiments in sound.
-
-        That means we accept work from genres including:
-        Sound and radio art
-        Experimental audio documentary
-        Plunderphonic and audio collage
-        Acoustic ecology and soundscapes
-        Sound rich fiction
-        Experimental interviews
-        Etc...
-
-        However, this isn’t a space for experimental music.
-
-        We aren’t a show in the traditional sense. There is no host, no weekly theme, no ads, no clock and no release schedule. Instead this is an ongoing space where different kinds of audio is perpetually being submitted and added to the catalogue. A playlist where you download or stream for as long as you like, listen actively, passively, on your commute or in your shower. No interruptions from DJs, hosts or ad breaks.
-      </Typography>
-      <Typography variant='display2'>
+      <Typography variant='display2' style={{marginBottom:'10px'}}>
         Who are we?
       </Typography>
-      <Typography variant='body2' style={{marginBottom:'20px'}}>
+      <Typography variant='body2' style={{marginBottom:'30px'}}>
         A couple of people who enjoy listening.
       </Typography>
-      <Typography variant='display2'>
+      <Typography variant='display2' style={{marginBottom:'10px'}}>
         What happens next?
      </Typography>
-     <Typography variant='body2' style={{marginBottom:'20px'}}>
+     <Typography variant='body2' style={{marginBottom:'30px'}}>
        We will review the submision to make sure it meets the guidelines and email you once it’s live. You can contact us at: <a href="mailto:longlivethenewsound@gmail.com">longlivethenewsound@gmail.com</a>.
         After you have submitted your piece, you will receive a confirmation email. We review each piece before it appears in the feed and will let you know when it appear on the show.
         Direct any questions to: <a href="mailto:longlivethenewsound@gmail.com">longlivethenewsound@gmail.com</a>
@@ -94,10 +77,10 @@ class App extends Component {
         If you hear something you want know more about, check the episode description for information provided by the uploader. If you want to support any of the work you find on this show, please find the artists directly.
      </Typography>
 
-     <Typography variant='display2'>
+     <Typography variant='display2' style={{marginBottom:'10px'}}>
       Additional Resources
     </Typography>
-    <Typography variant='body2' style={{marginBottom:'20px'}}>
+    <Typography variant='body2' style={{marginBottom:'30px'}}>
       Creative Audio Archive
       Ubu Web : Sound
       Earlid
