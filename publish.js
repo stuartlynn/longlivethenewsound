7f97d61d3254c59fb1ac1fae9939993224314c72
Stuart Lynn <stuart.lynn@gmail.com>
2018-06-03 14:34:47 -0400

    updating dummy data to be used by itunes

--- publish.js
+++ publish.js
@@ -3,19 +3,20 @@ var fs = require('fs');
 
 feed = new Podcast({
   title:'Long Live The New Sound',
-  description:'This is a podcast for artists, archivists, producers and other interested parties to share and hear work that experiments in sound. Submit your work at longlivethenewsound.com',
+  description:'This is a podcast for artists, archivists, producers and other interested parties to share and hear work that experiments in sound. Share work at longlivethenewsound.com',
   siteUrl:'longlivethenewsound.com',
   feed_url: 'https://s3.amazonaws.com/longlivethenewsound/rss.xml',
   imageUrl:'https://s3.amazonaws.com/longlivethenewsound/LLTNSLogo.jpg',
   author:'longlivethenewsound',
   itunesAuthor: 'longlivethenewsound',
+  language:'en-us',
   itunesSubtitle: 'A podcast for artists, archivists, producers and other interested parties to share and hear work that experiments in sound.',
   itunesOwner:{
     name:'Long Live The New Sound',
     email:'longlivethenewsound@gmail.com'
   },
   itunesExplicit: true,
-  itunesCategory: ['Personal Journals' ],
+  itunesCategory: [{text:'Society & Culture', subcats: [{text:'Personal Journals'}]}],
   itunesImage: 'https://s3.amazonaws.com/longlivethenewsound/LLTNSLogo.jpg',
   itunesType:''
 })
@@ -23,17 +24,25 @@ feed = new Podcast({
 feed.addItem({
   title:'FROGS',
   description: "Frogs recorded outside Puerto Maldonado, Peru",
-  url:'https://s3.amazonaws.com/longlivethenewsound/episodes/FROGS_forlltns.mp3',
+  url:'https://feed.longlivethenewsound.com/episodes/FROGS_forlltns.mp3',
+  enclosure:{url:'https://feed.longlivethenewsound.com/episodes/FROGS_forlltns.mp3',
+    file:'/Users/slynn/Downloads/Frogs_mini.mp3'
+  },
   guid:1,
-  date:'May 27, 2018'
+  date:'May 27, 2018',
+  itunesDuration:'00:00:45'
 });
 
 feed.addItem({
   title:'About Process',
   description: "Audio collage about audio collages" ,
-  url:'https://s3.amazonaws.com/longlivethenewsound/episodes/forlltns_process.mp3',
+  url:'https://feed.longlivethenewsound.com/episodes/forlltns_process.mp3',
+  enclosure:{url:'https://feed.longlivethenewsound.com/episodes/forlltns_process.mp3',
+    file:'/Users/slynn/Downloads/forlltns_process.mp3'
+  },
   guid:2,
-  date:'May 27, 2018'
+  date:'May 27, 2018',
+  itunesDuration:'00:03:23'
 });
 
 const xml = feed.buildXml();
