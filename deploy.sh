d7547a97b485cd701b4a2345f99d13102fca2d55
Stuart Lynn <stuart.lynn@gmail.com>
2018-06-05 17:03:00 -0400

    deplot script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git checkout deploy
+git merge --commit --no-edit master
+cd client
+yarn build
+cd ../
+git add client/build
+git commit -m "new deploy at $(date)"
+git push heroku deploy:master
+git checkout master
